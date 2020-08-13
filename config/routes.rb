c094311804bce67e01c15fb9d70c8a43b20193c1
Keith <ko.keith@hotmail.com>
2020-08-13 09:56:04 -0400

    add users stock controller, add front-end search stock function

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+
   devise_for :users
   get 'welcome/index' => 'welcome#index'
   
   root 'welcome#index'
   
+  get 'my_portfolio', to: "users#my_portfolio"
 
-  
+  get 'search_stock', to: "stocks#search"
 end
