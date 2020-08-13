36732a7722e19306d2e736c21d7b126d1b518a7a
Keith <ko.keith@hotmail.com>
2020-08-13 10:11:15 -0400

    add error handling for empty search and invalid search

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,11 +1,19 @@
 class StocksController < ApplicationController
 
-    
-
     def search
-        @stock = Stock.new_lookup(params[:stock])
+        if params[:stock].present?
+            @stock = Stock.new_lookup(params[:stock])
+            if @stock
+                render 'users/my_portfolio'
+            else
+                flash[:alert] = "Please enter a valid symbol"
+                redirect_to my_portfolio_path
+            end
+        else
+            flash[:alert] = "Please enter a symbol to search"
+            redirect_to my_portfolio_path
+        end
 
-        render 'users/my_portfolio'
     end
 
 end
