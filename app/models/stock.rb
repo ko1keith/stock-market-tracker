06e14c56783cf1975e112f696afd1895133a8400
Keith <ko.keith@hotmail.com>
2020-08-14 09:32:53 -0400

    add many to many associations b/w users and stocks

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,10 @@
 class Stock < ApplicationRecord
 
+    has_many :user_stocks
+    has_many :users, through: :user_stocks
+
+    validates :name, :ticker, presence: true
+
     def self.new_lookup(ticker_symbol)
         client = IEX::Api::Client.new(
             publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
@@ -11,5 +16,5 @@ class Stock < ApplicationRecord
             return nil
         end
     
-        end
+    end
 end
