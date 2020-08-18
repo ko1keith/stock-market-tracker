ccd2e4e7f1fc885741a0de102f3089e5e88b92a9
Keith <ko.keith@hotmail.com>
2020-08-18 11:19:16 -0400

    add search friends form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,25 @@ class UsersController < ApplicationController
   def my_friends
     @friends = current_user.friend
   end
+
+  def search 
+    if params[:friend].present?
+      @friend = (params[:friend])
+      if @friend
+          respond_to do |format|
+              format.js { render partial: 'users/friend_result'}
+          end
+      else
+          respond_to do |format|
+              flash.now[:alert] = "Please enter a valid friend or email"
+              format.js { render partial: 'users/friend_result'}
+          end
+      end
+    else
+      respond_to do |format|
+          flash.now[:alert] = "Please enter a friend"
+          format.js { render partial: 'users/friend_result'}
+      end
+    end
+  end
 end
