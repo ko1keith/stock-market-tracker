cce57ae06ee4b54d66cc70dd7ee8e8abea9d311d
Keith <ko.keith@hotmail.com>
2020-08-19 09:47:28 -0400

    update stock price when refresh page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,11 @@ class UsersController < ApplicationController
 
   def my_portfolio
     @tracked_stocks = current_user.stocks
+
+    @tracked_stocks.each do |tracked_stock|
+      tracked_stock.last_price = Stock.update_stock(tracked_stock.ticker)
+      tracked_stock.save
+    end
   end
   
   def my_friends
@@ -10,14 +15,17 @@ class UsersController < ApplicationController
 
   def search 
     if params[:friend].present?
-      @friend = (params[:friend])
-      if @friend
+      #return collection of search results
+      @friends = User.search(params[:friend])
+      #in this list, if exclude current_user(logged in user) if current_user is in collection
+      @friends = current_user.except_current_user(@friends)
+      if @friends
           respond_to do |format|
               format.js { render partial: 'users/friend_result'}
           end
       else
           respond_to do |format|
-              flash.now[:alert] = "Please enter a valid friend or email"
+              flash.now[:alert] = "Couldn't find user"
               format.js { render partial: 'users/friend_result'}
           end
       end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,21 +1,30 @@
 class Stock < ApplicationRecord
 
-    has_many :user_stocks
-    has_many :users, through: :user_stocks
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
 
-    validates :name, :ticker, presence: true
+  validates :name, :ticker, presence: true
 
-    def self.new_lookup(ticker_symbol)
-        client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
-                                      endpoint: 'https://sandbox.iexapis.com/v1')
-        begin
-          new(ticker: ticker_symbol.upcase, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
-        rescue => exception
-          return nil
-        end
+
+  def self.new_lookup(ticker_symbol)
+    client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
+                                  endpoint: 'https://sandbox.iexapis.com/v1')
+    begin
+      new(ticker: ticker_symbol.upcase, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+    rescue => exception
+      return nil
     end
-    
-    def self.check_db(ticker_symbol)
-        where(ticker: ticker_symbol).first
-    end
+  end
+  
+  def self.check_db(ticker_symbol)
+    where(ticker: ticker_symbol).first
+  end
+
+  def self.update_stock(ticker_symbol)
+    client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
+                                  endpoint: 'https://sandbox.iexapis.com/v1')
+
+    client.price(ticker_symbol)
+  end
+
 end
